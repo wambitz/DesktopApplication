d85016da3b8fc35174a04590dab7efc102523fb0
wambitz <wambee@hotmail.com>
2016-03-19 20:00:36 -0600

    Testing branch

--- structure.py
+++ structure.py
@@ -6,3 +6,5 @@ app = QApplication(sys.argv)    # Only one instance
 dialog = QDialog()              # window
 dialog.show()                   # Show window
 app.exec_()                     # exec_because reserved word in python
+
+print("Testing push in branch")
