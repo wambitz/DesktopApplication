cc3746467499ad06fc9f0140a4afaf9556ac776e
wambitz <wambee@hotmail.com>
2016-03-21 11:32:41 -0600

    Created "Hello world! widget

--- structure.py
+++ structure.py
@@ -2,9 +2,30 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import sys
 
+
+class HelloWorld(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        # super(HelloWorld, self).__init__() # overwrite constructor
+        # Container
+        layout = QVBoxLayout()
+
+        # Objects(widgets) within the container
+        label = QLabel("Hello World!")
+        line_edit = QLineEdit()
+        button = QPushButton("Close")
+
+        # Modify object properties
+        layout.addWidget(label)
+        layout.addWidget(line_edit)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+# -------------------------- MAIN # ---------------------------------
+# This code calls the main program
 app = QApplication(sys.argv)    # Only one instance 
-dialog = QDialog()              # window
+dialog = HelloWorld()              # window
 dialog.show()                   # Show window
 app.exec_()                     # exec_because reserved word in python
 
-print("Testing push in branch")
